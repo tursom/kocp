8523ca0cd9e4c683b51cf3bfc5b5ceee928b3ba2
tursom <tursom@foxmail.com>
2019-04-25 00:38:31 +0800

    测试

--- src/main/kotlin/cn/tursom/kocp/math/HashMap.kt
+++ src/main/kotlin/cn/tursom/kocp/math/HashMap.kt
@@ -4,22 +4,8 @@ import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
 import kotlin.collections.HashMap
 
-open class HashMap<K, V>(mapOf: Map<K, V> = mapOf()) : HashMap<K, V>(mapOf) {
+fun HashMap<*, *>.toJson() = Gson().toJson(this) ?: "{}"
 
-	fun toJson() = Gson().toJson(this) ?: "{}"
-
-	companion object {
-		inline fun <reified K, reified V : Any> fromJson(json: String): HashMap<K, V>? {
-			val hashMap = HashMap<K, V>()
-			return try {
-				val gson = Gson().fromJson(json, cn.tursom.kocp.math.HashMap::class.java)
-				gson.forEach {
-					hashMap[it.key as K] = (Gson().fromJson(it.value.toString(), V::class.java) ?: return null)
-				}
-				hashMap
-			} catch (e: JsonSyntaxException) {
-				null
-			}
-		}
-	}
+fun Gson.fromJsonMap(json: String): HashMap<*, *>? {
+	return fromJson(json, HashMap::class.java)
 }
