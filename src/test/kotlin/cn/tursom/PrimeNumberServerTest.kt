0bc1ad327ad512f432af6d6f7350f5276a7b8b7d
tursom <tursom@foxmail.com>
2019-05-09 01:43:51 +0800

    添加关于质数的UDP服务器

--- src/test/kotlin/cn/tursom/PrimeNumberServerTest.kt
+++ src/test/kotlin/cn/tursom/PrimeNumberServerTest.kt
@@ -5,8 +5,21 @@ import cn.tursom.server.PrimeNumberServer
 import cn.tursom.server.toByteArray
 import cn.tursom.server.toLong
 import org.junit.Test
+import java.net.SocketTimeoutException
 
 class PrimeNumberServerTest {
+	fun UdpClient.getPrimeNumber(num: Long) {
+		send(num.toByteArray(), 10_000) { bytes, size ->
+			println("$num ${if (bytes[0] == 0.toByte()) "不" else ""}是质数")
+			print("因数分解: ")
+			for (j in 1 until size step 8) {
+				print("${bytes.toLong(j)} ")
+			}
+			println()
+			println()
+		}
+	}
+	
 	@Test
 	fun test() {
 		val port = 12345
@@ -15,14 +28,12 @@ class PrimeNumberServerTest {
 		
 		UdpClient("127.0.0.1", port).use {
 			for (i in 1..100L) {
-				it.send(i.toByteArray(), 10_000) { bytes, size ->
-					println("$i ${if (bytes[0] == 0.toByte()) "不" else ""}是质数")
-					print("因数分解: ")
-					for (j in 1 until size step 8) {
-						print("${bytes.toLong(j)} ")
+				while (true) {
+					try {
+						it.getPrimeNumber(i)
+						break
+					} catch (e: SocketTimeoutException) {
 					}
-					println()
-					println()
 				}
 			}
 		}
